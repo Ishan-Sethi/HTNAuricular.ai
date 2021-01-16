45dd78b7365b519c2be8b215ee1cf96d198fb198
Min <63823931+MuteMini@users.noreply.github.com>
2021-01-16 13:14:27 -0500

    changed file name, add BasicFrame abstract class

--- MainApp.py
+++ MainApp.py
@@ -3,47 +3,46 @@ from tkinter import ttk
 from tkinter import filedialog
 from ttkthemes import ThemedStyle
 
-# MainFrame class to play around with
-# self.parent - MainApp variable
-class MainFrame(tk.Frame):
+class BasicFrame(ttk.Frame):
     def __init__(self, master, parent):
         tk.Frame.__init__(self, master)
         self.parent = parent
         self.create_widgets()
+        pass
+    # Supply this with widgets to add
+    def create_widgets(self):
+        raise NotImplementedError("Implement this create_widgets(), bumboclat! %s" %(self.__class__.__name__))
+    def hideWindow(self):
+        self.pack_forget()
+        pass
+    # Supply this with proper pack method
+    def showWindow(self):
+        raise NotImplementedError("Implement this showWindow(), bumboclat! %s" %(self.__class__.__name__))
 
+class MainFrame(BasicFrame):
     def create_widgets(self):
         self.hi_there = ttk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
-
         self.quit = ttk.Button(self, text="QUIT", width=30, command=lambda:[self.hideWindow(),self.parent.mainFrame2.showWindow()])
         self.quit.pack(side="bottom")
-
+        pass
     def say_hi(self):
         print("hi there, everyone!")
-
-    def hideWindow(self):
-        self.pack_forget()
+        pass
     def showWindow(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, padx=10, pady=10, expand=True)
-class MainFrameTwo(tk.Frame):
-    def __init__(self, master, parent):
-        tk.Frame.__init__(self, master)
-        self.parent = parent
-        self.create_widgets()
+        pass
 
+class MainFrameTwo(BasicFrame):
     def create_widgets(self):
         self.racism = ttk.Label(self, text="china", width=20)
         self.racism.pack(side="top")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-    def hideWindow(self):
-        self.pack_forget()
+        pass
     def showWindow(self):
         self.pack(side="top", fill="both", padx=100, pady=100, expand=True)
+        pass
 
 # Main Window Class
 class MainApp(object):
@@ -54,12 +53,13 @@ class MainApp(object):
         self.root.resizable(0, 0)
 
         self.style = ThemedStyle(self.root)
-        self.style.set_theme("yaru")
+        self.style.set_theme("adapta")
 
         self.mainFrame = MainFrame(self.root, self)
         self.mainFrame2 = MainFrameTwo(self.root, self)
 
         self.mainFrame.showWindow()
+        pass
 
 mainApp = MainApp()
 mainApp.root.mainloop()

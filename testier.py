fa8cdf2f66dfc81507d2a73cc395ce50b7c87fd2
Min <63823931+MuteMini@users.noreply.github.com>
2021-01-16 12:50:34 -0500

    created oop style main app control thing jargon jargon bla bla bla

--- testier.py
+++ testier.py
@@ -1,31 +1,65 @@
 import tkinter as tk
-import ttkthemes
+from tkinter import ttk
+from tkinter import filedialog
+from ttkthemes import ThemedStyle
 
-# find a good skin to use
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
+# MainFrame class to play around with
+# self.parent - MainApp variable
+class MainFrame(tk.Frame):
+    def __init__(self, master, parent):
+        tk.Frame.__init__(self, master)
+        self.parent = parent
         self.create_widgets()
-        self.pack()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
+        self.hi_there = ttk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit = ttk.Button(self, text="QUIT", width=30, command=lambda:[self.hideWindow(),self.parent.mainFrame2.showWindow()])
         self.quit.pack(side="bottom")
 
     def say_hi(self):
         print("hi there, everyone!")
 
-root = tk.Tk()
+    def hideWindow(self):
+        self.pack_forget()
+    def showWindow(self):
+        self.pack(side="top", fill="both", ipadx=30, ipady=30, padx=10, pady=10, expand=True)
+class MainFrameTwo(tk.Frame):
+    def __init__(self, master, parent):
+        tk.Frame.__init__(self, master)
+        self.parent = parent
+        self.create_widgets()
 
-root.title("gay program")
-root.geometry("800x500")
-root.resizable(0, 0)
+    def create_widgets(self):
+        self.racism = ttk.Label(self, text="china", width=20)
+        self.racism.pack(side="top")
 
-app = Application(master=root)
-app.mainloop()
+    def say_hi(self):
+        print("hi there, everyone!")
+
+    def hideWindow(self):
+        self.pack_forget()
+    def showWindow(self):
+        self.pack(side="top", fill="both", padx=100, pady=100, expand=True)
+
+# Main Window Class
+class MainApp(object):
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("gay program")
+        self.root.geometry("800x500")
+        self.root.resizable(0, 0)
+
+        self.style = ThemedStyle(self.root)
+        self.style.set_theme("yaru")
+
+        self.mainFrame = MainFrame(self.root, self)
+        self.mainFrame2 = MainFrameTwo(self.root, self)
+
+        self.mainFrame.showWindow()
+
+mainApp = MainApp()
+mainApp.root.mainloop()

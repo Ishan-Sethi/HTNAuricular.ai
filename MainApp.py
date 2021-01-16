026290ab52ef4ba9de2d3d9e50b12322dfa0a10d
Min <63823931+MuteMini@users.noreply.github.com>
2021-01-16 14:58:56 -0500

    aryan i lvoe you ,#

--- MainApp.py
+++ MainApp.py
@@ -7,6 +7,7 @@ class BasicFrame(ttk.Frame):
     def __init__(self, master, parent):
         tk.Frame.__init__(self, master)
         self.parent = parent
+        self.configure(background='#f5f6f7')
         self.create_widgets()
         pass
     # Supply this with widgets to add
@@ -21,16 +22,19 @@ class BasicFrame(ttk.Frame):
 
 class MainFrame(BasicFrame):
     def create_widgets(self):
-        self.hi_there = ttk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-        self.quit = ttk.Button(self, text="QUIT", width=30, command=lambda:[self.hideWindow(),self.parent.mainFrame2.showWindow()])
-        self.quit.pack(side="bottom")
+        self.heading = ttk.Label(self, text="Auricular.ai", style="heading.TLabel")
+        self.heading.pack(side="top", pady=(10,0))
+        self.addFile = ttk.Button(self, text="Process Recording", width=25, command=self.getFileLocation)
+        self.addFile.pack(side="top", pady=(50,0))
+        self.addFile = ttk.Button(self, text="Notes", width=25)
+        self.addFile.pack(side="top", pady=(30,0))
+        self.addFile = ttk.Button(self, text="Quit Program", width=25, command=self.parent.root.destroy)
+        self.addFile.pack(side="top", pady=(30,0))
         pass
-    def say_hi(self):
-        print("hi there, everyone!")
-        pass
+
+    def getFileLocation(self):
+        print(filedialog.askopenfilename(filetypes=[("Image files", ".png .jpg .jpeg")]))
+
     def showWindow(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, padx=10, pady=10, expand=True)
         pass
@@ -53,7 +57,10 @@ class MainApp(object):
         self.root.resizable(0, 0)
 
         self.style = ThemedStyle(self.root)
-        self.style.set_theme("adapta")
+        self.style.set_theme("arc")
+
+        self.ttkstyle = ttk.Style()
+        self.ttkstyle.configure('heading.TLabel', font='TkDefaultFont 32')
 
         self.mainFrame = MainFrame(self.root, self)
         self.mainFrame2 = MainFrameTwo(self.root, self)

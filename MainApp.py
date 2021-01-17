515dc6664eb3ac2f430ad717687846cd1318e133
Min <63823931+MuteMini@users.noreply.github.com>
2021-01-16 20:26:47 -0500

    splash screen edited

--- MainApp.py
+++ MainApp.py
@@ -1,3 +1,6 @@
+import random
+import time
+import math
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -16,21 +19,47 @@ class BasicFrame(ttk.Frame):
     # Supply this with widgets to add
     def create_widgets(self):
         raise NotImplementedError("Implement this create_widgets(), bumboclat! %s" %(self.__class__.__name__))
-    def hideWindow(self):
+    def hide_window(self):
         self.pack_forget()
         pass
     # Supply this with proper pack method
-    def showWindow(self):
-        raise NotImplementedError("Implement this showWindow(), bumboclat! %s" %(self.__class__.__name__))
+    def show_window(self):
+        raise NotImplementedError("Implement this show_window(), bumboclat! %s" %(self.__class__.__name__))
 
 # Aryan complete this
 class SplashFrame(BasicFrame):
-    def create_widgets(self):
-        self.back = ttk.Button(self, text="Back", width=25, command=lambda:[self.hideWindow(),self.parent.mainFrame.showWindow()])
-        self.back.pack(side="bottom", pady=(0,100))
-        pass
 
-    def showWindow(self):
+    def create_widgets(self):
+        self.canvas = tk.Canvas(self, width=800, height=500)
+        self.canvas.pack()
+        print("function has run create")
+        pass
+    def reset_animation(self):
+        self.counter = 0 #delta but never changes
+        self.x, self.y, self.a, self.m = 145, -110, 550, -510
+        self.xspeed, self.yspeed = 1, 1 #movement variable x and y
+        self.title = self.canvas.create_text(self.x, self.y, anchor='w', font=("TkMenuFont",75), text="Auricular.AI")
+        print("function has run")
+        pass
+    def run_animation(self):
+        print(self.counter)
+        self.counter+=1
+        if self.counter < 324:
+            self.y+=1
+            self.canvas.move(self.title,0,self.yspeed) #Adds to the coordinates of line
+        elif self.counter >= 324 and self.counter <= 590:
+            self.a-=1
+            self.canvas.create_rectangle(-10, self.a, 800, self.a+300, outline = "#add8e6", fill = "#add8e6")
+        elif self.counter >= 590 and self.counter <= 692:
+            self.m+=5
+            self.canvas.create_rectangle(-10, self.m, 810, self.m+500, outline = "#f5f6f7", fill = "#f5f6f7")
+        if self.counter == 693:
+            self.hide_window()
+            self.parent.mainFrame.show_window()
+        else:
+            self.master.after(5, self.run_animation)
+        pass
+    def show_window(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, expand=True)
         pass
 
@@ -38,19 +67,19 @@ class SplashFrame(BasicFrame):
 class MainFrame(BasicFrame):
     def create_widgets(self):
         self.heading = ttk.Label(self, text="Auricular.ai", style="heading.TLabel")
-        self.heading.pack(side="top", pady=(20,0))
-        self.record = ttk.Button(self, text="Process Recording", width=25, command=self.getFileLocation)
+        self.heading.pack(side="top", pady=(0,50), ipady=(20,0))
+        self.record = ttk.Button(self, text="Process Recording", width=25, command=self.get_file_location)
         self.record.pack(side="top", pady=(50,0))
-        self.note = ttk.Button(self, text="Notes", width=25, command=lambda:[self.hideWindow(),self.parent.notesFrame.showWindow()])
+        self.note = ttk.Button(self, text="Notes", width=25, command=lambda:[self.hide_window(),self.parent.notesFrame.show_window()])
         self.note.pack(side="top", pady=(30,0))
         self.quit = ttk.Button(self, text="Quit Program", width=25, command=self.parent.root.destroy)
         self.quit.pack(side="top", pady=(30,0))
         pass
 
-    def getFileLocation(self):
+    def get_file_location(self):
         print(filedialog.askopenfilename(filetypes=[("Image files", ".png .jpg .jpeg")]))
 
-    def showWindow(self):
+    def show_window(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, expand=True)
         pass
 
@@ -59,7 +88,7 @@ class LoadingFrame(BasicFrame):
     def create_widgets(self):
         pass
 
-    def showWindow(self):
+    def show_window(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, expand=True)
         pass
 
@@ -68,18 +97,18 @@ class OutputFrame(BasicFrame):
     def create_widgets(self):
         pass
 
-    def showWindow(self):
+    def show_window(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, expand=True)
         pass
 
 # Show all saved notes and open em up
 class NotesFrame(BasicFrame):
     def create_widgets(self):
-        self.back = ttk.Button(self, text="Back", width=25, command=lambda:[self.hideWindow(),self.parent.mainFrame.showWindow()])
+        self.back = ttk.Button(self, text="Back", width=25, command=lambda:[self.hide_window(),self.parent.mainFrame.show_window()])
         self.back.pack(side="bottom", pady=(0,100))
         pass
 
-    def showWindow(self):
+    def show_window(self):
         self.pack(side="top", fill="both", ipadx=30, ipady=30, expand=True)
         pass
 
@@ -97,10 +126,13 @@ class MainApp(object):
         self.ttkstyle = ttk.Style()
         self.ttkstyle.configure('heading.TLabel', font='TkDefaultFont 32')
 
+        self.splashFrame = SplashFrame(self.root, self)
         self.mainFrame = MainFrame(self.root, self)
         self.notesFrame = NotesFrame(self.root, self)
 
-        self.mainFrame.showWindow()
+        self.splashFrame.show_window()
+        self.splashFrame.reset_animation()
+        self.splashFrame.run_animation()
         pass
 
 mainApp = MainApp()

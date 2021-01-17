924602dd8f9db829d6147216edfd5ec8e06acb62
Ishan-Sethi <35640149+Ishan-Sethi@users.noreply.github.com>
2021-01-16 23:20:21 -0500

    Minor bug fixes

--- WitBackend.py
+++ WitBackend.py
@@ -3,16 +3,19 @@ import json
 import AudioUpload
 from wit import Wit
 
+def pretty_print(response_json: json):
+    print(json.dumps(response_json, indent=4, sort_keys=True))
 
 def sendResponses(responses):
     client = Wit('C56EAX6MPKJ5NDESA5RZ5JIUPGC5NDZP')
     data = []
-    for result in responses:
-        response = client.message(result)
-        print(str(response))
-        data.append(str(response))
+    for sentence in responses:
+        for phrase in sentence:
+            response = client.message(phrase)
+            pretty_print(response)
+            data.append(str(response))
     return data
 
-responses = AudioUpload.sendAudio("Recording_2.wav")
+responses = AudioUpload.sendAudio("./test audio/aryan is teacher 1.wav")
 print (responses)
 print(sendResponses(responses))
